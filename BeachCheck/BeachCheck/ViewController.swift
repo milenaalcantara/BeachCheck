0c0883cd1ad2dae718165c92c201192d16d88818
Milena Alcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-10-10 11:44:20 -0300

    base app

---
+++ BeachCheck/BeachCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeachCheck
+//
+//  Created by Milena Lima de Alcântara on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
